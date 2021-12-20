0b5399b2cd5c2408d453c806328328b61407650d
Tiana21 <andrianovonatiana@gmail.com>
2021-12-20 09:35:16 +0300

    config url,et vue post_list

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
+# url =>  vue, izay miseo amin n ecran
+#  base url = 127.0.01:8000
+# ex1
+#  127.0.01:8000/admin
+#                   io : admin.py 
+# 127.0.01:8000
+#            si  juste base url: view.postlite
+# faceboook = base url
+# facebook/profiletiana = url hafa
